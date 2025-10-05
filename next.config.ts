7d19d006ce277fe4bbddd3f87994c062aa9722be
remusito <rcerezomartin@gmail.com>
2025-10-05 16:30:28 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -32,7 +33,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-  }
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
